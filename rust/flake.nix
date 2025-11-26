1f7ae2663f0f79ce7f2b23a10bdc1eb9fb90301b
Songpol Anannetikul <ice.songpola@pm.me>
2025-11-26 14:53:29 +0700

    refactor(rust): migrate to flakelight

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,38 +1,29 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flakelight = {
+      url = "github:nix-community/flakelight";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
   outputs =
-    inputs@{
-      flake-parts,
-      nixpkgs,
-      rust-overlay,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem =
-        { pkgs, system, ... }:
-        {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-            ];
-          };
-          devShells.default =
-            with pkgs;
-            mkShell {
-              packages = [
-                (rust-bin.stable.latest.default.override {
-                  extensions = [ "rust-src" ];
-                })
-              ];
-            };
+    { flakelight, rust-overlay, ... }@inputs:
+    flakelight ./. {
+      inherit inputs;
+      withOverlays = [
+        rust-overlay.overlays.default
+      ];
+      devShell =
+        pkgs: with pkgs; {
+          packages = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+          ];
         };
     };
 }
