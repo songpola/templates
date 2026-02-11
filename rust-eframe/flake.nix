44cf77cb6ddcc7c152a889e5ae2e6d4967d56e37
Songpol Anannetikul <ice.songpola@pm.me>
2026-02-11 09:41:55 +0700

    refactor: update rust-eframe template

--- rust-eframe/flake.nix
+++ rust-eframe/flake.nix
@@ -1,62 +1,54 @@
 {
-  description = "eframe devShell";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs =
-    inputs@{
-      flake-parts,
-      nixpkgs,
-      rust-overlay,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem =
-        { pkgs, system, ... }:
-        {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-            ];
-          };
-          devShells.default =
-            with pkgs;
-            mkShell rec {
-              packages = [
-                # Rust
-                (rust-bin.stable.latest.default.override {
-                  extensions = [ "rust-src" ];
-                })
-                trunk
-
-                # misc. libraries
-                openssl
-                pkgconf
-
-                # GUI libs
-                libxkbcommon
-                libGL
-                fontconfig
-
-                # wayland libraries
-                wayland
-
-                # x11 libraries
-                xorg.libXcursor
-                xorg.libXrandr
-                xorg.libXi
-                xorg.libX11
-              ];
-
-              LD_LIBRARY_PATH = lib.makeLibraryPath packages;
-            };
-        };
+    flakelight = {
+      url = "github:nix-community/flakelight";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    { flakelight, rust-overlay, ... }@inputs:
+    flakelight ./. (
+      { lib, ... }:
+      {
+        inherit inputs;
+        withOverlays = [
+          rust-overlay.overlays.default
+        ];
+        devShell = pkgs: rec {
+          packages = with pkgs; [
+            # Rust
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            trunk
+
+            # misc. libraries
+            openssl
+            pkgconf
+
+            # GUI libs
+            libxkbcommon
+            libGL
+            fontconfig
+
+            # wayland libraries
+            wayland
+
+            # x11 libraries
+            libxcursor
+            libxrandr
+            libxi
+            libx11
+          ];
+          env = {
+            LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+          };
+        };
+      }
+    );
 }
