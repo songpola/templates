7b9bae6cb8f1a2d78e2c0f8aa36ae63186690cb8
Songpol Anannetikul <ice.songpola@pm.me>
2026-03-05 15:29:08 +0700

    feat: add python template

--- flake.nix
+++ flake.nix
@@ -13,14 +13,21 @@
           path = ./rust;
           description = "Rust development environment (using oxalica/rust-overlay)";
         };
-        # https://github.com/emilk/eframe_template/
         rust-eframe = {
           path = ./rust-eframe;
-          description = "Based on emilk/eframe_template, with flake-parts and oxalica/rust-overlay";
+          description = "Rust eframe template (based on emilk/eframe_template)";
         };
         typst = {
           path = ./typst;
           description = "Typst development environment (using RossSmyth/press)";
+        };
+        python = {
+          path = ./python;
+          description = "Python development environment (using Pixi)";
+          welcomeText = ''
+            - `pixi init`: Initialize a new Pixi project in the current directory.
+            - `pixi add python`: Add Python interpreter to the project.
+          '';
         };
       };
     };
